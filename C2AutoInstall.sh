2c1eda72e932f392288d7ba40d233771732424af
assume-breach <76174163+assume-breach@users.noreply.github.com>
2021-07-29 09:02:57 -0600

    Update C2AutoInstall.sh

--- C2AutoInstall.sh
+++ C2AutoInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+banner()
+{ __ _ ___ ___ _   _ _ __ ___   ___      | |__  _ __ ___  __ _  ___| |__  
+ / _` / __/ __| | | | '_ ` _ \ / _ \_____| '_ \| '__/ _ \/ _` |/ __| '_ \ 
+| (_| \__ \__ \ |_| | | | | | |  __/_____| |_) | | |  __/ (_| | (__| | | |
+ \__,_|___/___/\__,_|_| |_| |_|\___|     |_.__/|_|  \___|\__,_|\___|_| |_|
+}
 apt-get update -y && apt-get upgrade -y
 
 apt install python3.7 python3.7-dev python3-pip
@@ -97,6 +102,10 @@ wget https://github.com/Ne0nd0g/merlin/releases/download/v0.8.0/merlinServer-Lin
 cd /opt
 
 apt install postgresql -y
+
+systemctl start postgresql 
+
+systemctl enable postgresql
 
 apt install curl -y
 
